37312b7595992b258182eb632ed4af55dabca213
henry kirya <henry.kirya@gmail.com>
2019-07-25 20:16:30 +0300

    erste commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+import {controll} from './controller.js';
+
+
+var stuff1 = document.getElementById("cont1");
+var stuff2 = document.getElementById("cont2");
+var stuff3 = document.getElementById("cont3");
+const taskcontroll  = new controll(stuff1, stuff2, stuff3);
+
+document.querySelector('#one').addEventListener('click', ff1);
+document.querySelector('#two').addEventListener('click', ff2);
+document.querySelector('#three').addEventListener('click', ff3);
+
+
+function ff1() {
+	taskcontroll.tog('one');
+}
+function ff2() {
+	taskcontroll.tog('two');
+}
+function ff3() {
+	taskcontroll.tog('three');
+}
+
